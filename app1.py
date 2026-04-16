b7e4e7dfe968d7117f6e30e4081c3598e495d715
Thanh Tùng <dangthanhtung020312@gmail.com>
2026-04-16 23:36:42 +0700

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+# Trang chủ - Lý thuyết (400 dòng của bạn)
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Trang bài tập - Trắc nghiệm & Tự luận (500 dòng của bạn)
+@app.route('/bai-tap')
+def baitap():
+    return render_template('bai-tap.html')
+
+if __name__ == '__main__':
+    # Render sẽ cung cấp cổng PORT tự động thông qua biến môi trường
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
